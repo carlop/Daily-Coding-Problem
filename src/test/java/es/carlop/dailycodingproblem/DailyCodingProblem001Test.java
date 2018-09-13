ab12e5d7a704f225fbde815c3a86d5a4f1444fce
Carlos López <yo@carlop.es>
2018-09-13 09:13:36 +0200

    Refactored the test a litle
    Now each problem will have it's own test file.

---
+++ src/test/java/es/carlop/dailycodingproblem/DailyCodingProblem001Test.java
@@ -0,0 +1,46 @@
+package es.carlop.dailycodingproblem;
+
+import es.carlop.dailycodingproblem.Problem001.Problem001;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for each Daily Coding Problem
+ */
+@DisplayName("Problem 001")
+public class DailyCodingProblem001Test {
+
+    int[] exampleOne;
+    int[] exampleTwo;
+    int kOne = 17;
+    int kTwo = 11;
+
+    @BeforeEach
+    public void SetUp() {
+        exampleOne = new int[] {10, 15, 3, 7};
+        exampleTwo = new int[] {9, 14, 2, 4};
+    }
+
+    @Test
+    @DisplayName("True example")
+    public void TestTrue() {
+        // Given example
+        assertTrue(Problem001.addUpToK(exampleOne, kOne));
+
+        // Another example
+        assertTrue(Problem001.addUpToK(exampleTwo, kTwo));
+    }
+
+    @Test
+    @DisplayName("False example")
+    public void TestFalse() {
+        // Given example
+        assertFalse(Problem001.addUpToK(exampleOne, kTwo));
+
+        // Another example
+        assertFalse(Problem001.addUpToK(exampleTwo, kOne));
+    }
+}
